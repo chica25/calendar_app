8d87d8f85a2668b50694cf9cbe2fb503399efda8
chica25 <pfco520@gmail.com>
2021-06-07 00:08:13 -0400

    Assign for loops to include the current date

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const date = new Date();
-// console.log(date)
 
-// const month = date.getMonth()
-// console.log(month)
+date.setDate(1);
+
+// console.log(date.getDay())
 
 const monthDays = document.querySelector(".days");
 
-const lastDay = new Date(date.getFullYear(), date.getMonth() + 1);
-console.log(lastDay)
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+// console.log(lastDay)
+const prevLastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+// console.log(prevLastDay)
+const firstDayIndex = date.getDate();
+
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+// console.log(lastDayIndex)
+const nextDays = 7 - lastDayIndex - 1;
 
 const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 // console.log(months)
@@ -18,7 +25,22 @@ document.querySelector(".date p").innerHTML = date.toDateString();
 
 let days = "";
 
-for(let i = 1; i <= 31; i++) {
-    days += `<div>${i}</div>`;
+for(let x = firstDayIndex; x > 0; x --) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+}
+
+for(let i = 1; i <= lastDay; i++) {
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+        days += `<div class="today">${i}</div>`;
+    } else {
+        days += `<div>${i}</div>`;
+    }
+   
+}
+
+for(let j = 1; j <= nextDays; j ++) {
+    days += '<div class="next-date">${j}</div>';
     monthDays.innerHTML = days;
 }
+
+
